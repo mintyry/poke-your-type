8b31ebd84b409630fb6ccb8c3e48b2f59188b6f7
mintyry <gitmintyry@gmail.com>
2023-12-13 11:12:36 -0800

    able to get array of names by types

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
-
-
-// function fetchPokemon() {
-//     //https://pokeapi.co/api/v2/type/: array[0-17] are the types; photo in images folder
-//     for (let id = 1; id <= 18; id++) {
-//         let url = `https://pokeapi.co/api/v2/type/${id}`;
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(data => {
-//                 //we fetch each type's url id, each url id contains type name and all the pokemon in that type.
-//                 //type/id produces data. data.name shows type. data.pokemon show all pokemon
-//                 console.log(data.pokemon[0])
-//                 for (let i = 0; i <= data.length; i++) {
-
-//                     console.log(data.pokemon[i])
-//                     // const type = data.name;
-//                     // const pokemonForThatType = data.pokemon
-
-//                 }//ends for loop
-//             })//ends .thendata
-//     }//ends id for loop
-// }//ends function
-
-
 function fetchPokemon() {
+
+    let pokemonByType = {}
+    let fetchCount = 0
     //https://pokeapi.co/api/v2/type/: array[0-17] are the types; photo in images folder
+    for (let id = 1; id <= 18; id++) {
+        let url = `https://pokeapi.co/api/v2/type/${id}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                //we fetch each type's url id, each url id contains type name and all the pokemon in that type.
+                //type/id produces data. data.name shows type. data.pokemon show all pokemon
+                // console.log(data.pokemon[1])
+                let typeArray = []
+                for (let i = 0; i < data.pokemon.length; i++) {
+                    typeArray.push(data.pokemon[i].pokemon.name)
 
-    let url = `https://pokeapi.co/api/v2/type/1`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            //we fetch each type's url id, each url id contains type name and all the pokemon in that type.
-            //type/id produces data. data.name shows type. data.pokemon show all pokemon
-            // console.log(data.pokemon[1])
-            let normalPkmn = []
-            for (let i = 0; i < data.pokemon.length; i++) {
-                normalPkmn.push(data.pokemon[i].pokemon.name)
+                }//ends for loop
 
-            }//ends for loop
-            console.log(normalPkmn);
-        })//ends .thendata
-}
+                //by each url id, i log an array of all pokemon in that url id/type.
+                //So this inner loop makes one array, but the outer for loop repeats the process fot each url, so when we log it, we will see each array from each url (stored in typeArray).
+                // Then we store all of these arrays in one object
+
+                pokemonByType[id] = typeArray;
+                fetchCount++;
+
+                if (fetchCount === 18) {
+                    console.log(pokemonByType[2])
+                }
+
+            })//ends .thendata
+
+    }//ends id for loop
+}//ends fetchPokemon fn
 
 
 fetchPokemon();
-
