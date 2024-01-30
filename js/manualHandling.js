7cf625c59aa7d7e6c816f381efe41599182c24d0
mintyry <gitmintyry@gmail.com>
2024-01-29 22:04:46 -0800

    moved og artwork into own folder

--- js/manualHandling.js
+++ js/manualHandling.js
@@ -17,42 +17,42 @@ function manualHandle(name, type) {
         }
         // no photos in api for following pokemon
         case 'sinistcha': {
-            let pokemonImg = './images/sinistcha.png';
+            let pokemonImg = './images/ogartwork/sinistcha.png';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Sinistcha';
             break
         }
         case 'ogerpon-wellspring': {
-            let pokemonImg = './images/wellspring.webp';
+            let pokemonImg = './images/ogartwork/wellspring.webp';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Ogerpon';
             break
         }
         case 'ogerpon-hearthflame': {
-            let pokemonImg = './images/hearthflame.webp';
+            let pokemonImg = './images/ogartwork/hearthflame.webp';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Ogerpon';
             break
         }
         case 'ogerpon-cornerstone': {
-            let pokemonImg = './images/cornerstone.webp';
+            let pokemonImg = './images/ogartwork/cornerstone.webp';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Ogerpon';
             break
         }
         case 'shellos-east': {
-            let pokemonImg = './images/shellos-east.png';
+            let pokemonImg = './images/ogartwork/shellos-east.png';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Shellos';
             break
         }
         case 'gastrodon-east': {
-            let pokemonImg = './images/gastrodon-east.png';
+            let pokemonImg = './images/ogartwork/gastrodon-east.png';
             let imgDiv = document.querySelector(`#${type}-img`)
             imgDiv.src = pokemonImg;
             document.querySelector(`#${type}-name`).textContent = 'Gastrodon';
