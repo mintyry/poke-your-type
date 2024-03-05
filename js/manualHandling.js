faed6bd62c809d2f9a4c400e1c6b6bfb5a4f812d
mintyry <gitmintyry@gmail.com>
2024-03-04 17:32:36 -0800

    new pokemon are now fetchable

--- js/manualHandling.js
+++ js/manualHandling.js
@@ -18,50 +18,8 @@ function manualHandle(name, type, isShiny, currentShinyHandler) {
             return 'mimikyu-disguised'
         }
         // no photos in api for following pokemon
-        case 'hydrapple': {
-            if (currentShinyHandler) {
-                let shinyToggles = document.querySelectorAll(`#${type} .shiny`);
-                shinyToggles.forEach((shinyToggle) => {
-                    shinyToggle.removeEventListener('click', currentShinyHandler);
-                    // technically wrong to pass in parameters
-                    //can use the data; .bind() to correctly pass without unintentionally triggering shiny function
-                })
-            };
-            console.log(currentShinyHandler)
-            let pokemonImg = './images/ogartwork/hydrapple.png';
-            let shinyImg = './images/ogartwork/shiny_hydrapple.png';
-
-            let shinyToggles = document.querySelectorAll(`#${type} .shiny`);
-            currentShinyHandler = shiny(isShiny, pokemonImg, shinyImg, type);
-
-            // add functionality to each button
-            shinyToggles.forEach((shinyToggle) => {
-                shinyToggle.addEventListener('click', currentShinyHandler);
-            });
-            updateInfo(type, 'Hydrapple', isShiny, pokemonImg, shinyImg);
-            break
-        }
-        case 'archaludon': {
-            if (currentShinyHandler) {
-                let shinyToggles = document.querySelectorAll(`#${type} .shiny`);
-                shinyToggles.forEach((shinyToggle) => {
-                    shinyToggle.removeEventListener('click', currentShinyHandler);
-                    // technically wrong to pass in parameters
-                    //can use the data; .bind() to correctly pass without unintentionally triggering shiny function
-                })
-                currentShinyHandler = null;
-            };
-            console.log(currentShinyHandler)
-            let pokemonImg = './images/ogartwork/archaludon.png';
-            let shinyImg = './images/ogartwork/shiny_archaludon.png';
-            updateInfo(type, 'Archaludon', isShiny, pokemonImg, shinyImg, currentShinyHandler);
-            break
-        }
-        case 'terapagos': {
-            let pokemonImg = './images/ogartwork/terapagos.png';
-            updateInfo(type, 'Terapagos', pokemonImg);
-            break
-        }
+    
+       
         case 'terapagos-terestal': {
             let pokemonImg = './images/ogartwork/terapagos-terestal.png';
             updateInfo(type, 'Terapagos', pokemonImg);
@@ -72,41 +30,7 @@ function manualHandle(name, type, isShiny, currentShinyHandler) {
             updateInfo(type, 'Terapagos', pokemonImg);
             break
         }
-        case 'pecharunt': {
-            let pokemonImg = './images/ogartwork/pecharunt.png';
-            updateInfo(type, 'Pecharunt', pokemonImg);
-            break
-        }
-        case 'iron-crown': {
-            let pokemonImg = './images/ogartwork/iron-crown.png';
-            updateInfo(type, 'Iron Crown', pokemonImg);
-            break
-        }
-        case 'iron-boulder': {
-            let pokemonImg = './images/ogartwork/iron-boulder.png';
-            updateInfo(type, 'Iron Boulder', pokemonImg);
-            break
-        }
-        case 'raging-bolt': {
-            let pokemonImg = './images/ogartwork/raging-bolt.png';
-            updateInfo(type, 'Raging Bolt', pokemonImg);
-            break
-        }
-        case 'gouging-fire': {
-            let pokemonImg = './images/ogartwork/gougingfire.png';
-            updateInfo(type, 'Gouging Fire', pokemonImg);
-            break
-        }
-        case 'sinistcha': {
-            let pokemonImg = './images/ogartwork/sinistcha.png';
-            updateInfo(type, 'Sinistcha', pokemonImg);
-            break
-        }
-        case 'ogerpon-wellspring': {
-            let pokemonImg = './images/ogartwork/wellspring.png';
-            updateInfo(type, 'Ogerpon', pokemonImg);
-            break
-        }
+      
         case 'ogerpon-hearthflame': {
             let pokemonImg = './images/ogartwork/hearthflame.png';
             updateInfo(type, 'Ogerpon', pokemonImg);
